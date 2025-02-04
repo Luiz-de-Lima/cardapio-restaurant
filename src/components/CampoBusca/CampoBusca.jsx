7d913794f54fd0d0ca44f60e8e4bcf87710d2df7
Luiz-de-Lima <luiz12359@gmail.com>
2025-02-04 14:22:13 -0300

    feat: versão mobile dos components categorias e campo busca concluidos

--- src/components/CampoBusca/CampoBusca.jsx
+++ src/components/CampoBusca/CampoBusca.jsx
@@ -1,9 +1,7 @@
-import React, { useState } from "react";
 import estilos from "./styles.module.css";
 import Image from "next/image";
 import lupa from "../../../public/lupa.png";
-import { buscarProduto } from "@/services/services";
-// para fazer:filtrar os produtos através do botão busca e retorna a pesquisa
+
 export const CampoBusca = ({ onBuscar, textoBuscado }) => {
   return (
     <div className={estilos.campoBusca_container}>
